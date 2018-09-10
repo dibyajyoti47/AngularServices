f782430958f38671ef721463f187f3ebcf157f84
Dibyajyoti Behera <dibyajyoti.a.behera@accenture.com>
2018-09-10 17:14:31 +0530

    Course completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AddBookComponent } from './add-book/add-book.component';
 import { AddReaderComponent } from './add-reader/add-reader.component';
@@ -9,6 +10,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { EditBookComponent } from './edit-book/edit-book.component';
 import { EditReaderComponent } from './edit-reader/edit-reader.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -22,9 +24,10 @@ import { EditReaderComponent } from './edit-reader/edit-reader.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    CoreModule,
+    HttpClientModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Version, VERSION } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { Title } from '@angular/platform-browser';
 
 import { Book } from "app/models/book";
-import { allBooks, allReaders } from 'app/data';
 import { Reader } from "app/models/reader";
+import { LoggerService } from 'app/core/logger.service';
+import { DataService } from 'app/core/data.service';
+import { BookTrackerError } from "app/models/bookTrackerError";
 
 @Component({
   selector: 'app-dashboard',
@@ -11,13 +15,39 @@ import { Reader } from "app/models/reader";
 })
 export class DashboardComponent implements OnInit {
 
-  allBooks: Book[] = allBooks;
-  allReaders: Reader[] = allReaders;
-  mostPopularBook: Book = allBooks[0];
+  allBooks: Book[];
+  allReaders: Reader[];
+  mostPopularBook: Book;
 
-  constructor() { }
+  constructor(private loggerService: LoggerService,
+              private dataService: DataService,
+              private title: Title) {
+    this.loggerService.log('Creating the dashboard.');
+  }
 
   ngOnInit() {
+    this.allBooks = this.dataService.getAllBooks();
+    this.dataService.getAllReaders()
+      .subscribe(
+        (data: Reader[]) => this.allReaders = data,
+        (err: BookTrackerError) => console.log(err.friendlyMessage),
+        () => this.loggerService.log('All done getting readers!')
+      );
+    this.mostPopularBook = this.dataService.mostPopularBook;
+
+    this.getAuthorRecommendationAsync(1)
+      .catch(err => this.loggerService.error(err));
+
+    this.title.setTitle(`Book Tracker ${VERSION.full}`);
+
+    this.loggerService.log('Done with dashboard initialization.');
+
+    throw new Error('Ugly technical error!');
+  }
+
+  private async getAuthorRecommendationAsync(readerID: number): Promise<void> {
+    let author: string = await this.dataService.getAuthorRecommendation(readerID);
+    this.loggerService.log(author);
   }
 
   deleteBook(bookID: number): void {
